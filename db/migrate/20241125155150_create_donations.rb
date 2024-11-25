d31c5fee8ec1e03460ee7d6bee59d0898fc5cb59
Jaime Barros <jabgnecco@gmail.com>
2024-11-25 15:54:38 +0000

    added models to the project

---
+++ db/migrate/20241125155150_create_donations.rb
@@ -0,0 +1,14 @@
+class CreateDonations < ActiveRecord::Migration[7.1]
+  def change
+    create_table :donations do |t|
+      t.references :user, null: false, foreign_key: true
+      t.references :project, null: false, foreign_key: true
+      t.float :amount
+      t.string :state
+      t.string :donation_sku
+      t.string :checkout_session_id
+
+      t.timestamps
+    end
+  end
+end
