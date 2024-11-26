75f884066be21b911892fcd632a26b2a686fcdb9
Jaime Barros <jabgnecco@gmail.com>
2024-11-26 17:47:04 +0000

    stripe payments and donation/new page with simple form for donation

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -18,12 +18,26 @@ class DonationsController < ApplicationController
     @donation.user = current_user
     @donation.state = "pending"
     @donation.save
-    redirect_to new_payment_path(donation_id: @donation.id)
+
+    session = Stripe::Checkout::Session.create(
+      project_method_types: ['card'],
+      line_items: [{
+        name: @donation.donation_sku,
+        amount: @donation.amount,
+        currency: 'gbp',
+        quantity: 1
+        }],
+        succes_url: donation_url(@donation),
+        cancel_url: donation_url(@donation)
+      )
+
+      @donation.update(checkout_session_id: session.id)
+      redirect_to new_payment_path(donation_id: @donation.id)
   end
 
   private
 
   def donation_params
-    params.require(:donation).permit(:amount, :state, :donation_sku, :checkout_session_id)
+    params.require(:donation).permit(:amount, :donation_sku)
   end
 end
