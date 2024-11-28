528ff88f64f1d7754bbeaee90fcf8f072b7ebc0b
Jaime Barros <jabgnecco@gmail.com>
2024-11-28 17:41:10 +0000

    removed images

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema[7.1].define(version: 2024_11_28_160821) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,10 +40,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_11_28_160821) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
-
-ActiveRecord::Schema[7.1].define(version: 2024_11_28_141120) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  end
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -52,7 +48,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_11_28_141120) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["project_id"], name: "index_categories_on_project_id"
-
   end
 
   create_table "donations", force: :cascade do |t|
