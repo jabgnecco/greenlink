f52907a6cbf2b20be23adc0ba03fef26580eba02
Jaime Barros <jabgnecco@gmail.com>
2024-11-29 16:28:32 +0000

    ...

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
   def my_donations
-    @donations = current_user.donations.where(state: "paid")
+    @donations = current_user.donations.where(state: "Paid")
   end
 end
