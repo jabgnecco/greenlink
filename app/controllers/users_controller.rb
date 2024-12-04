df6f89d32517b9afcf62e619f10adaf3b965ec94
nicorepetto <nicojrepetto@gmail.com>
2024-12-04 11:16:52 +0000

    fixing profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,8 @@
 class UsersController < ApplicationController
   def profile
-    @donations = current_user.donations.where(state: "paid")
+    @donations = current_user.donations
     @projects = current_user.projects.order(created_at: :desc)
   end
 
-  
+
 end
